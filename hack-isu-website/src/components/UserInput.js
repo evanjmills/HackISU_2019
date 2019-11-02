7d4d5e511882ca3823bfa4ff132e1ba0bef12493
Maxewell Farver <mfarver99@gmail.com>
2019-11-02 16:24:03 -0500

    Fixing some things, nothing major

--- hack-isu-website/src/components/UserInput.js
+++ hack-isu-website/src/components/UserInput.js
@@ -4,30 +4,44 @@ export const UserInput = () => (
   <div className='user-input-wrapper'>
     <div className='user-input'>
       <h2>Make A Choice</h2>
-      <form action=''>
+      <form>
         <div>
           <label htmlFor='name'>Name</label>
           <input type='text' name='name' id='' />
         </div>
-        <div>
-          <label htmlFor='param1'>Param1</label>
-          <input type='text' name='param1' id='' />
-        </div>
-        <div>
-          <label htmlFor='param2'>Param2</label>
-          <input type='text' name='param2' id='' />
-        </div>
-        <div>
-          <label htmlFor='param3'>Param3</label>
-          <input type='text' name='param3' id='' />
-        </div>
-        <div>
-          <label htmlFor='param4'>Param4</label>
-          <input type='text' name='param4' id='' />
-        </div>
-        <div>
-          <label htmlFor='param5'>Param5</label>
-          <input type='text' name='param5' id='' />
+        <div className='user-options-wrapper'>
+          <div>
+            <label htmlFor='numHidden'>numHidden</label>
+            <input type='text' name='numHidden' id='' />
+          </div>
+          <div>
+            <label htmlFor='survival'>survival</label>
+            <input type='text' name='survival' id='' />
+          </div>
+          <div>
+            <label htmlFor='nodeAdd'>nodeAdd</label>
+            <input type='text' name='nodeAdd' id='' />
+          </div>
+          <div>
+            <label htmlFor='nodeDel'>nodeDel</label>
+            <input type='text' name='nodeDel' id='' />
+          </div>
+          <div>
+            <label htmlFor='connAdd'>connAdd</label>
+            <input type='text' name='connAdd' id='' />
+          </div>
+          <div>
+            <label htmlFor='connDel'>connDel</label>
+            <input type='text' name='connDel' id='' />
+          </div>
+          <div>
+            <label htmlFor='weightAdd'>weightAdd</label>
+            <input type='text' name='weightAdd' id='' />
+          </div>
+          <div>
+            <label htmlFor='weightDel'>weightDel</label>
+            <input type='text' name='weightDel' id='' />
+          </div>
         </div>
       </form>
     </div>
