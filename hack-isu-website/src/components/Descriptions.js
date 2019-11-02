90d746e7a674fa7db8dbc3d3c9ff00d447fe4e41
Maxewell Farver <mfarver99@gmail.com>
2019-11-02 13:18:49 -0500

    Some frontend changes

--- hack-isu-website/src/components/Descriptions.js
+++ hack-isu-website/src/components/Descriptions.js
@@ -9,8 +9,8 @@ export const Descriptions = () => (
         <p>Total number of hidden nodes.</p>
       </div>
       <div>
-        <h3>Survival Threshold</h3>
-        <p>Percentage of models that "survive" each generation.</p>
+        <h3>Node Add Probability</h3>
+        <p>Probability that a hidden node is added to the model.</p>
       </div>
       <div>
         <h3>Connection Add Probability</h3>
@@ -19,24 +19,28 @@ export const Descriptions = () => (
         </p>
       </div>
       <div>
+        <h3>Weight Mutation Rate</h3>
+        <p>Probability that a random connection weight will be mutated.</p>
+      </div>
+      <div>
+        <h3>Survival Rate</h3>
+        <p>
+          Percentage of a species that survives from one generation to the next.
+        </p>
+      </div>
+      <div>
+        <h3>Node Delete Probability</h3>
+        <p>Probability that a hidden node is deleted from the model.</p>
+      </div>
+
+      <div>
         <h3>Connection Delete Probability</h3>
         <p>
           Probability that an existing connection is deleted between existing
           nodes.
         </p>
       </div>
-      <div>
-        <h3>Node Add Probability</h3>
-        <p>Probability that a hidden node is added to the model.</p>
-      </div>
-      <div>
-        <h3>Node Delete Probability</h3>
-        <p>Probability that a hidden node is deleted from the model.</p>
-      </div>
-      <div>
-        <h3>Weight Mutation Rate</h3>
-        <p>Probability that a random connection weight will be mutated.</p>
-      </div>
+
       <div>
         <h3>Weight Replacement Rate</h3>
         <p>Probability that a random connection weight will be replaced.</p>
@@ -44,3 +48,14 @@ export const Descriptions = () => (
     </div>
   </div>
 );
+
+// <div>
+//         <h3>Node Delete Probability</h3>
+//         <p>Probability that a hidden node is deleted from the model.</p>
+//       </div>
+//       <div>
+//         <h3>Connection Add Probability</h3>
+//         <p>
+//           Probability that a new connection is added between existing nodes.
+//         </p>
+//       </div>
