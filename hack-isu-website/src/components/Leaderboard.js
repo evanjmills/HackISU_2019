578e5f72ae0c0ee85a3555d2bfeac3d68e506d7b
Maxewell Farver <mfarver99@gmail.com>
2019-11-01 23:47:13 -0500

    Rough outline of site, barely started styling

---
+++ hack-isu-website/src/components/Leaderboard.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export const Leaderboard = () => (
+  <div className='leaderboard'>
+    <h2>Leaderboard</h2>
+    <table>
+      <thead>
+        <td>Name</td>
+        <td>Score</td>
+      </thead>
+      <tr>
+        <td>name1</td>
+        <td>1</td>
+      </tr>
+      <tr>
+        <td>name2</td>
+        <td>2</td>
+      </tr>
+      <tr>
+        <td>name3</td>
+        <td>3</td>
+      </tr>
+    </table>
+  </div>
+);
