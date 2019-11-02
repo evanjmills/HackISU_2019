03004a323b18ed0059ebfad0667fa2bfb608199e
Maxewell Farver <mfarver99@gmail.com>
2019-11-02 03:09:28 -0500

    Most styling done, on to writing the backend

--- hack-isu-website/src/components/Leaderboard.js
+++ hack-isu-website/src/components/Leaderboard.js
@@ -4,20 +4,24 @@ export const Leaderboard = () => (
   <div className='leaderboard-wrapper'>
     <div className='leaderboard'>
       <h2>Leaderboard</h2>
-      <table>
+      <table cellSpacing='0'>
         <thead>
+          <td>Rank</td>
           <td>Name</td>
           <td>Score</td>
         </thead>
         <tr>
+          <td>1</td>
           <td>name1</td>
           <td>1</td>
         </tr>
         <tr>
+          <td>2</td>
           <td>name2</td>
           <td>2</td>
         </tr>
         <tr>
+          <td>3</td>
           <td>name3</td>
           <td>3</td>
         </tr>
