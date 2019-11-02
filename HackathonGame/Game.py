f1d845197cc606444185caaf2e6c09e5c75e6957
Greyson A. Latinwo Jones <GreysonLatinwo@gmail.com>
2019-11-01 21:45:50 -0500

    attack and defend is dun

--- HackathonGame/Game.py
+++ HackathonGame/Game.py
@@ -7,41 +7,58 @@ size = Window_Width, Window_Height = 640, 480
 fullWindow = pygame.display.set_mode(size)
 
 clock = pygame.time.Clock()
-FPS = 144
+FPS = 300
 
 playerColor = (255, 255, 255)
+GREEN = (0, 127, 0)
+RED = (255, 0, 0)
 BLACK = (0, 0, 0)
 playerPos = [Window_Width//2, Window_Height//2]
-player = pygame.display.set_mode(size)
 
 
 def move(keys, event):
     if event.type == pygame.KEYDOWN or sum(keys) > 0:
-        if keys[pygame.K_w]:
+        if keys[pygame.K_UP]:
             playerPos[1] -= 1
-        if keys[pygame.K_a]:
+        if keys[pygame.K_LEFT]:
             playerPos[0] -= 1
-        if keys[pygame.K_s]:
+        if keys[pygame.K_DOWN]:
             playerPos[1] += 1
-        if keys[pygame.K_d]:
+        if keys[pygame.K_RIGHT]:
             playerPos[0] += 1
 
+
 def attack():
-    pygame.draw.circle(fullWindow, playerColor, (playerPos[0], playerPos[1]), 30, 5)
+    pygame.draw.circle(fullWindow, GREEN, (playerPos[0], playerPos[1]), 30)
+    pygame.display.update()
+
+
+def defend():
+    pygame.draw.circle(fullWindow, RED, (playerPos[0], playerPos[1]), 30)
+    pygame.display.update()
+
 
 def game():
     while 1:
         keys = pygame.key.get_pressed()
         clock.tick(FPS)
+        defending = True
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-        move(keys, event)
-        attack()
+        if event.type == pygame.KEYDOWN or sum(keys) > 0:
+            if keys[pygame.K_x]:
+                attack()
+        if event.type == pygame.KEYDOWN or sum(keys) > 0:
+            if keys[pygame.K_z]:
+                defend()
+                defending = False
 
+        if defending:
+            move(keys, event)
         pygame.Surface.fill(fullWindow, BLACK)
         pygame.draw.circle(fullWindow, playerColor, (playerPos[0], playerPos[1]), 20)
         pygame.display.update()
 
-
+pygame.draw.rect()
 game()
